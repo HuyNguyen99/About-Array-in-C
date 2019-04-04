e9a98215f1925f5cf62d9c69da67d40de6753afe
HuyNguyen99 <48156395+HuyNguyen99@users.noreply.github.com>
2019-04-04 14:38:16 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void sapxepArray(int array[], int kichthuoc);
+int main()
+{
+    int array[10]={3,4,1,6,8,3,5,8,9,2};
+    sapxepArray(array, 10);
+    return 0;
+}
+void sapxepArray(int array[], int kichthuoc)
+{
+    int i=0;
+    int a=0;
+    while(i<10)
+    {
+        if(array[i]==a)
+        {
+            printf("%d", a);
+        }
+        if(i==9)
+        {
+            a++;
+            i=0;
+        }
+        i++;
+    }
+    printf("\nThe end!");
+}
